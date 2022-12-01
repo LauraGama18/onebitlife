1dae8b96f5b1a6661562e0cd3f8ab616b5829262
Laura Gama <lauragama18@yahoo.com.brm>
2022-12-01 09:34:43 -0300

    Estrutura de páginas e rotas

---
+++ scr/pages/start/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {VIEW, ScrollView, Text, Image } from "react-native-gesture-handler";
+
+
+export default function Start() {
+
+return {
+    <VIEW>
+        <ScrollView showVertivalScrollIndicator={false}>
+
+    <view>
+    <Image 
+    source={require("../../../../../../assets/icons/logo3.png")}
+    />
+    <Text>
+        Vamos transformar sua vida {"/n"} em jogo , buscando sempre {"/n"} o melhor nível.
+    </Text>
+    </view>
+        </ScrollView>
+    </VIEW>
+}
+
+}
